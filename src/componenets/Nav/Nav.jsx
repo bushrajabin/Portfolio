476dd6e171a14cf07ce3175054acdf664bc0e041
Bushra jabin <bushrabintjabin@gmail.com>
2024-01-06 10:37:31 +0530

    Added scrollbar

--- src/componenets/Nav/Nav.jsx
+++ src/componenets/Nav/Nav.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
-
+	
 
 const Nav = () => {
+<link
 
+
+/>
 
     return (
         <div className='  flex flex-row  justify-between p-5 cursor-pointer fixed w-full bg-white  xl:flex xl:flex-row xl:justify-between xl:p-5 xl:text-center xl:items-center xl:cursor-pointer xl:fixed  xl:w-full xl:z-0 xl:bg-white  '>
@@ -11,7 +14,7 @@ const Nav = () => {
             </div>
 
             <div className=" font-bold space-x-6 xl:text-1xl xl:font-bold xl:space-x-12 ">
-                <a href="">About</a>
+                <a href="/about">About</a>
                 <a href="">Notes</a>
                 <a href="" >Contact</a>
             </div>
